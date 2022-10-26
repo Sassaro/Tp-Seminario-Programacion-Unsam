cd16d737ffe919a9dda225242037030464fb4ebe
Sassaro <tomassassaro@gmail.com>
2022-10-26 12:00:44 -0300

    Initial Commit

---
+++ Place.py
@@ -0,0 +1,6 @@
+class Place:
+
+    def __init__(self, conexionesEntrada, conexionesSalida, tokens):
+        self.name = conexionesEntrada
+        self.age = conexionesSalida
+        self.tokens = tokens
---
+++ Transition.py
@@ -0,0 +1,5 @@
+class Trasition:
+
+    def __init__(self, conexionesEntrada, conexionesSalida):
+        self.name = conexionesEntrada
+        self.age = conexionesSalida
